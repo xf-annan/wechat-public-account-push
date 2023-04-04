9f381bf822bb33c08bf4e68e32c1aa62e5e4316f
xf-annan <62022259+xf-annan@users.noreply.github.com>
2023-04-04 18:31:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,13 +8,13 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx6ff0c7a84130470b',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '6262f8b4ae04306003f98ba30ff5471c',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '宁夏',
+  CITY: '银川',
 
   USERS: [
     {
